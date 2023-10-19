53d6185571a31e58549fc72327eaddff5bc7b964
Erfan-Hamidi <erfan9757@gmail.com>
2023-10-19 17:15:31 +0330

    add rtcpeer

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,25 @@
 let localstream;
 let remotestream;
+let peerconnection;
+
 
 let init = async ()  => {
     localstream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
     document.getElementById("user-1").srcObject = localstream
+}
+
+
+let createOffer = async () => {
+    peerconnection = new RTCPeerConnection()
+
+    remotestream = new MediaStream()
+    document.getElementById('user-2').srcObject = remotestream
+    
+    let offer =  await peerconnection.createOffer()
+    await peerconnection.setLocalDescription(offer)
+
+    console.log('offer:', offer)
+
 }
 
 init()
