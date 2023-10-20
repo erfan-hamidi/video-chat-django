f90aa10a82c2471ae5836d97df4247b6403edd9d
Erfan-Hamidi <erfan9757@gmail.com>
2023-10-20 17:59:19 +0330

    add stun server and track

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,13 @@ let localstream;
 let remotestream;
 let peerconnection;
 
+const servers = {
+    iceServers:[
+        {
+            urls:['stun1.l.google.com:19302','stun2.l.google.com:19302']
+        }
+    ]
+}
 
 let init = async ()  => {
     localstream = await navigator.mediaDevices.getUserMedia({video:true, audio:true})
@@ -10,11 +17,22 @@ let init = async ()  => {
 
 
 let createOffer = async () => {
-    peerconnection = new RTCPeerConnection()
+    peerconnection = new RTCPeerConnection(servers)
 
     remotestream = new MediaStream()
     document.getElementById('user-2').srcObject = remotestream
     
+
+    localstream.getTracks().forEach((track) => {
+        peerconnection.addTrack(track, localstream)
+    })
+
+    peerconnection.ontrack = (event) => {
+        event.streams[0].getTracks().forEach((track) => {
+            remotestream.addTrack(track)
+        })
+    }
+
     let offer =  await peerconnection.createOffer()
     await peerconnection.setLocalDescription(offer)
 
